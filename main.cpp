4c0a2cb2ad473e86ba7a6d7f085123756dd6630c
chi-chi1 <chiamakaokereke31@gmail.com>
2022-02-28 13:50:42 +0000

    enter yor name program

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+#include <array>
 #include <cstdio>
 
-int main() { std::printf("%s\n", "Hello World"); }
+int main()
+{
+        std::array<char, 32> name {};
+        std::printf("enter your name");
+        std::scanf("%s", name.begin());
+        std::printf("%s/n", name.begin());
+
+
+
+            
+}
